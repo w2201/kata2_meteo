1f5b2eaeab9cba75f7484e95c278cc583823640a
w2201 <w22012025@gmail.com>
2026-03-16 03:34:38 +0100

    add basename to router

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Weather from "../page/Weather";
 
-export const router = createBrowserRouter([
-  { path: "/", element: <Weather /> },
-]);
+export const router = createBrowserRouter(
+  [{ path: "/", element: <Weather /> }],
+  { basename: "/hata2_meteo" },
+);
